082b2a8503561f03e5d9130219f6d8f7f60aaa8d
Simone Taeggi <startae14@gmail.com>
2018-11-14 17:18:23 +0100

    strings and methods exercise

--- strings.py
+++ strings.py
@@ -1,6 +1,65 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Brad'
+age = 37
+# Concatenate
+print('Hello World ' + name + ' and I am ' + str(age))
 
 # String Formatting
 
+# Arguments by position
+#print('{}, {}, {}'.format('a', 'b', 'c') )
+print('{1}, {2}, {0}'.format('a', 'b', 'c') )
+
+# Arguments by name
+print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# F-Strings (only in 3.6+)
+print(f'My name is {name} and I am {age}')
+
 # String Methods
+
+s = 'hello there world'
+
+# Capitalize first letter
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lower
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get length
+print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = "h"
+print(s.count(sub))
+
+# Starts with
+print(s.startswith('hello'))
+
+# Ends with
+print(s.endswith('hello'))
+
+# Split into a list
+print(s.split())
+
+# Find position
+print(s.find('r'))
+
+# Is all alphanumeric
+print(s.isalnum())
+
+# is all alphabetic
+print(s.isalpha())
+
+# is all numeric
+print(s.isnumeric())
